c8d2d09c3c36309e74c70651556b416487bdd98e
Theosuper <theo.graczyk@gmail.com>
2026-05-24 16:08:11 -0300

    segunda lista e par/impar

--- cadastro.js
+++ cadastro.js
@@ -17,27 +17,101 @@ const jaCadastrados = [
     sells: 81231223,
     protagonist: "Donkey Kong",
   },
+  {
+    name: "Final Fantasy",
+    year: 2006,
+    sells: 1231223,
+    protagonist: "Vaan",
+  },
+];
+const div = document.getElementById("tablebody");
+jaCadastrados.map((item, index) => {
+  let par = index % 2 == 0;
+  let impar = index % 2 == 1;
+  if (par) {
+  }
+  if (impar) {
+    console.log = indice;
+  }
+  const row = document.createElement("tr");
+
+  const indice = document.createElement("th");
+  indice.textContent = `${index}`;
+  indice.classList = "indice";
+  row.appendChild(indice);
+
+  const name = document.createElement("th");
+  name.textContent = `${item.name}`;
+  name.classList = "name";
+  row.appendChild(name);
+
+  const sells = document.createElement("th");
+  sells.textContent = `${item.sells}`;
+  sells.classList = "sells";
+  row.appendChild(sells);
+
+  const protagonist = document.createElement("th");
+  protagonist.textContent = `${item.protagonist}`;
+  protagonist.classList = "protagonist";
+  row.appendChild(protagonist);
+
+  const year = document.createElement("th");
+  year.textContent = `${item.year}`;
+  year.classList = "year";
+  row.appendChild(year);
+
+  const actions = document.createElement("th");
+  const deletarBtn = document.createElement("button");
+  deletarBtn.textContent = "Deletar";
+  const editarBtn = document.createElement("button");
+  editarBtn.textContent = "Editar";
+  actions.appendChild(deletarBtn);
+  actions.appendChild(editarBtn);
+  actions.classList = "actions";
+  row.appendChild(actions);
+
+  div.appendChild(row);
+});
+
+const jedis = [
+  {
+    name: "Yoda",
+    lightsaber: "green",
+    power: 10,
+  },
+  {
+    name: "Cal Kestis",
+    lightsaber: "yellow",
+    power: 8,
+  },
+  {
+    name: "Obi wan Kenobi",
+    lightsaber: "blue",
+    power: 9,
+  },
+  {
+    name: "Mace Windu",
+    lightsaber: "purple",
+    power: 10,
+  },
 ];
 
-const div = document.getElementById("content");
-jaCadastrados.map((item, index) => {
-  const title = document.createElement("h2");
-  title.classList.add("name");
-  title.textContent = `${index + 1}. ${item.name}`;
-  div.appendChild(title);
+const div = document.getElementById("tablebody");
+jedis.map((item, index) => {
+  const row = document.createElement("tr");
 
-  const year = document.createElement("h2");
-  year.classList.add("year");
-  year.textContent = `${item.year}`;
-  div.appendChild(year);
+  const name = document.createElement("th");
+  name.textContent = `${item.name}`;
+  name.classList = "name";
+  row.appendChild(name);
 
-  const sells = document.createElement("h2");
-  sells.classList.add("sells");
-  sells.textContent = `${item.sells}`;
-  div.appendChild(sells);
+  const lightsaber = document.createElement("th");
+  lightsaber.textContent = `${item.lightsaber}`;
+  lightsaber.classList = "lightsaber";
+  row.appendChild(lightsaber);
 
-  const protagonist = document.createElement("h2");
-  protagonist.classList.add("protagonist");
-  protagonist.textContent = `${item.protagonist}`;
-  div.appendChild(protagonist);
+  const power = document.createElement("th");
+  power.textContent = `${item.power}`;
+  power.classList = "power";
+  row.appendChild(power);
 });
